003c3b87d065a4302704405ad06b68094eefbb87
Sochukwumaobim <ugwusochukwuma@gmail.com>
2026-08-16 21:40:55 +0200

    Delete surveymanagement directory

--- surveymanagement/survey_entry_dialog.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Compatibility module - redirects to main dialog
-"""
-
-from .SurveyManagement_dialog import SurveyManagementDialog as SurveyEntryDialog
-
-__all__ = ['SurveyEntryDialog']
