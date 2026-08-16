2b5431f82a618b13422a4bc74aec5fe6bb8b29aa
Sochukwumaobim <ugwusochukwuma@gmail.com>
2026-08-16 22:14:27 +0200

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -12,10 +12,7 @@
 
 /***************************************************************************
  *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
+ *   Released under the MIT License. See LICENSE for full terms.          *
  *                                                                         *
  ***************************************************************************/
  This script initializes the plugin, making it known to QGIS.
